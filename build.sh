abddd4e730843601732d97d3d779d6b04991467f
Ubuntu <owdorpwm@server-test.legends.systems>
2023-11-18 12:41:31 +0000

    updated build script

--- build.sh
+++ build.sh
@@ -13,40 +13,45 @@ echo "=======================">>setup.log 2>>error.log
 echo "Detecting IP Address"
 if [ -z "$LIVE_SERVER_IP" ]; then
     echo "Need to set LIVE_SERVER_IP"
-    exit 1
+    echo -n "Enter the live servers IP: "
+    read liveIP
+    LIVE_SERVER_IP=liveIP
 fi  
 echo "Detected IP Address is $LIVE_SERVER_IP"
 
 echo "Detecting Port"
 if [ -z "$LIVE_SERVER_PORT" ]; then
     echo "Need to set LIVE_SERVER_PORT"
-    exit 1
+    echo -n "Enter the live servers Port: "
+    read livePort
+    LIVE_SERVER_PORT=livePort
 fi
 echo "Detected Port is $LIVE_SERVER_PORT"
 
 echo "Detecting Nginx Port"
 if [ -z "$NGINX_LISTEN_PORT" ]; then
     echo "Need to set NGINX_LISTEN_PORT"
-    exit 1
+    echo -n "Enter the Nginx servers Port: "
+    read nginxPort
+    NGINX_LISTEN_PORT=nginxPort
 fi
 echo "Detected Port is $NGINX_LISTEN_PORT"
 
 echo "Detecting Cache FQDN URL"
 if [ -z "$CACHE_SERVER_FQDN_URL" ]; then
     echo "Need to set CACHE_SERVER_FQDN_URL"
-    exit 1
+    echo -n "Enter the Cache servers FQDN(url): "
+    read cacheURL
+    CACHE_SERVER_FQDN_URL=cacheURL
 fi
 echo "Detected URL is $CACHE_SERVER_FQDN_URL"
 
-SERVERADDRESS=$LIVE_SERVER_IP
-SERVERPORT=$LIVE_SERVER_PORT
-
-echo "Remove incompatible docker versions"
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+echo "Removing incompatible docker versions"
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg -y; done
 
 echo "Adding Docker's official GPG key"
 sudo apt-get update >>setup.log 2>>error.log
-sudo apt-get install ca-certificates curl gnupg
+sudo apt-get install ca-certificates curl gnupg -y
 sudo install -m 0755 -d /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
 sudo chmod a+r /etc/apt/keyrings/docker.gpg
@@ -59,7 +64,7 @@ echo \
 sudo apt-get update >>setup.log 2>>error.log
 
 echo "Installing docker and other needed Repositories"
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin nano git 
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin nano git -y
 
 echo "Enabling Docker Service"
 systemctl daemon-reload >>setup.log 2>>error.log

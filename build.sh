a9c0754659d280c5efde269e78f537a82b6f66ef
Wittyphantom333 <adam@wittsgarage.com>
2023-11-18 13:17:48 +0000

    updated build script

--- build.sh
+++ build.sh
@@ -15,7 +15,7 @@ if [ -z "$LIVE_SERVER_IP" ]; then
     echo "Need to set LIVE_SERVER_IP"
     echo -n "Enter the live servers IP: "
     read liveIP
-    $LIVE_SERVER_IP=liveIP
+    export LIVE_SERVER_IP=liveIP
 fi
 echo "Detected IP Address is $LIVE_SERVER_IP"
 
@@ -24,7 +24,7 @@ if [ -z "$LIVE_SERVER_PORT" ]; then
     echo "Need to set LIVE_SERVER_PORT"
     echo -n "Enter the live servers Port: "
     read livePort
-    $LIVE_SERVER_PORT=livePort
+    export LIVE_SERVER_PORT=livePort
 fi
 echo "Detected Port is $LIVE_SERVER_PORT"
 
@@ -33,7 +33,7 @@ if [ -z "$NGINX_LISTEN_PORT" ]; then
     echo "Need to set NGINX_LISTEN_PORT"
     echo -n "Enter the Nginx servers Port: "
     read nginxPort
-    $NGINX_LISTEN_PORT=nginxPort
+    export NGINX_LISTEN_PORT=nginxPort
 fi
 echo "Detected Port is $NGINX_LISTEN_PORT"
 
@@ -42,30 +42,30 @@ if [ -z "$CACHE_SERVER_FQDN_URL" ]; then
     echo "Need to set CACHE_SERVER_FQDN_URL"
     echo -n "Enter the Cache servers FQDN(url): "
     read cacheURL
-    $CACHE_SERVER_FQDN_URL=cacheURL
+    export CACHE_SERVER_FQDN_URL=cacheURL
 fi
 echo "Detected URL is $CACHE_SERVER_FQDN_URL"
 
 echo "Removing incompatible docker versions"
-for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg -y; done
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg -y; done >>setup.log 2>>error.log
 
 echo "Adding Docker's official GPG key"
 sudo rm /etc/apt/keyrings/docker.gpg >>setup.log 2>>error.log
 sudo apt-get update >>setup.log 2>>error.log
-sudo apt-get install ca-certificates curl gnupg -y
-sudo install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg 
-sudo chmod a+r /etc/apt/keyrings/docker.gpg
+sudo apt-get install ca-certificates curl gnupg -y >>setup.log 2>>error.log
+sudo install -m 0755 -d /etc/apt/keyrings >>setup.log 2>>error.log
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg >>setup.log 2>>error.log
+sudo chmod a+r /etc/apt/keyrings/docker.gpg >>setup.log 2>>error.log
 
 echo "Adding the repository to Apt sources"
 echo \
   "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
-  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null >>setup.log 2>>error.log
 sudo apt-get update >>setup.log 2>>error.log
 
 echo "Installing docker and other needed Repositories"
-sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin nano git -y
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin nano git -y >>setup.log 2>>error.log
 
 echo "Enabling Docker Service"
 systemctl daemon-reload >>setup.log 2>>error.log
@@ -73,26 +73,26 @@ systemctl enable docker.service >>setup.log 2>>error.log
 systemctl restart docker.service >>setup.log 2>>error.log
 
 echo "Setting nginx.conf values"
-sed -i "s/__LIVE_SERVER_IP__/$LIVE_SERVER_IP/g" files/nginx.conf
-sed -i "s/__LIVE_SERVER_PORT__/$LIVE_SERVER_PORT/g" files/nginx.conf
+sed -i "s/__LIVE_SERVER_IP__/$LIVE_SERVER_IP/g" files/nginx.conf >>setup.log 2>>error.log
+sed -i "s/__LIVE_SERVER_PORT__/$LIVE_SERVER_PORT/g" files/nginx.conf >>setup.log 2>>error.log
 
 echo "Setting docker-compose.yaml values"
-sed -i "s/__LIVE_SERVER_IP__/$LIVE_SERVER_IP/g" docker-compose.yml
-sed -i "s/__LIVE_SERVER_PORT__/$LIVE_SERVER_PORT/g" docker-compose.yml
-sed -i "s/__NGINX_LISTEN_PORT__/$NGINX_LISTEN_PORT/g" docker-compose.yml
-sed -i "s/__CACHE_SERVER_FQDN_URL__/$CACHE_SERVER_FQDN_URL/g" docker-compose.yml
+sed -i "s/__LIVE_SERVER_IP__/$LIVE_SERVER_IP/g" docker-compose.yml >>setup.log 2>>error.log
+sed -i "s/__LIVE_SERVER_PORT__/$LIVE_SERVER_PORT/g" docker-compose.yml >>setup.log 2>>error.log
+sed -i "s/__NGINX_LISTEN_PORT__/$NGINX_LISTEN_PORT/g" docker-compose.yml >>setup.log 2>>error.log
+sed -i "s/__CACHE_SERVER_FQDN_URL__/$CACHE_SERVER_FQDN_URL/g" docker-compose.yml >>setup.log 2>>error.log
 
 echo "Setting sites-availble.conf values"
-sed -i "s/__LIVE_SERVER_IP__/$LIVE_SERVER_IP/g" files/sites-available.conf 
-sed -i "s/__LIVE_SERVER_PORT__/$LIVE_SERVER_PORT/g" files/sites-available.conf 
-sed -i "s/__NGINX_LISTEN_PORT__/$NGINX_LISTEN_PORT/g" files/sites-available.conf 
-sed -i "s/__CACHE_SERVER_FQDN_URL__/$CACHE_SERVER_FQDN_URL/g" files/sites-available.conf
+sed -i "s/__LIVE_SERVER_IP__/$LIVE_SERVER_IP/g" files/sites-available.conf >>setup.log 2>>error.log
+sed -i "s/__LIVE_SERVER_PORT__/$LIVE_SERVER_PORT/g" files/sites-available.conf >>setup.log 2>>error.log
+sed -i "s/__NGINX_LISTEN_PORT__/$NGINX_LISTEN_PORT/g" files/sites-available.conf >>setup.log 2>>error.log
+sed -i "s/__CACHE_SERVER_FQDN_URL__/$CACHE_SERVER_FQDN_URL/g" files/sites-available.conf >>setup.log 2>>error.log
 
 echo "Adding user to docker group"
-sudo usermod -aG docker $USER
+sudo usermod -aG docker $USER >>setup.log 2>>error.log
 
 echo "Starting caching container"
-docker compose up --build -d
+docker compose up --build -d >>setup.log 2>>error.log
 
 echo "Installation has completed!!"
 
